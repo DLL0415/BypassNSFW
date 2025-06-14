b70a1df3ffec2aede6aa42380bd43d4b8f4bfff9
Danielle <daniell041510@gmail.com>
2025-06-14 15:02:18 -0400

    Create NSFWGateBypass.plugin.js

---
+++ NSFWGateBypass.plugin.js
@@ -0,0 +1,44 @@
+/**
+ * @name BypassNSFW
+ * @author DLL0415
+ * @description Lets you bypass NSFW/18+ channels.
+ * @version 1.0
+
+ */
+
+module.exports = class BypassNSFW {
+    start() {
+        this.patchNSFWCheck();
+    }
+
+    stop() {
+        BdApi.Patcher.unpatchAll("BypassNSFW");
+    }
+
+    patchNSFWCheck() {
+        const GuildModule = BdApi.findModuleByProps("getChannels", "getVoiceChannelId");
+        const PermissionsModule = BdApi.findModuleByProps("can", "getChannelPermissions");
+
+        const UserSettingsProto = BdApi.findModule((m) =>
+            typeof m?.default?.getCurrentUser === "function" &&
+            typeof m?.default?.getCurrentUser()?.nsfwAllowed !== "undefined"
+        )?.default?.getCurrentUser?.().__proto__;
+
+        if (UserSettingsProto) {
+            BdApi.Patcher.instead("BypassNSFW", UserSettingsProto, "nsfwAllowed", (_, __, orig) => {
+                return true;
+            });
+        }
+
+        const UserStore = BdApi.findModuleByProps("getCurrentUser");
+
+        if (UserStore && UserStore.getCurrentUser) {
+            BdApi.Patcher.after("BypassNSFW", UserStore, "getCurrentUser", (_, __, user) => {
+                if (user && user.hasOwnProperty("nsfwAllowed")) user.nsfwAllowed = true;
+                if (user && user.hasOwnProperty("ageVerified")) user.ageVerified = true;
+                if (user && user.hasOwnProperty("age")) user.age = 21;
+                if (user && user.hasOwnProperty("ageGateBypass")) user.ageGateBypass = true;
+            });
+        }
+    }
+};
